2470492e33e81e9a95d047afd28d46d01ecc0782
Vizonex <114684698+Vizonex@users.noreply.github.com>
2026-03-26 12:00:44 -0500

    upload tests

---
+++ tests/test_reduce.py
@@ -0,0 +1,101 @@
+import pytest
+
+from reducable_params._reduce_c import reduce as reduce_c
+from reducable_params._reduce_py import reduce as reduce_py
+from reducable_params.abc import Reducable, is_reducable
+
+
+@pytest.fixture(
+    params=[
+        (85022, 43727, 128749),
+        (14444, 84793, 99237),
+        (54194, 7697, 61891),
+        (89370, 55869, 145239),
+        (81581, 85501, 167082),
+        (64190, 90931, 155121),
+        (24752, 78572, 103324),
+        (31516, 8151, 39667),
+        (67001, 96812, 163813),
+        (31638, 95508, 127146),
+    ]
+)
+def addition_cases(request: pytest.FixtureRequest) -> tuple[int, ...]:
+    return request.param
+
+
+class BaseTestReduce:
+    reduce: type[Reducable]
+
+    def make_test_1(self) -> Reducable[[int, str | None], None]:
+        def sig(a: str, b: int | None = None):
+            pass
+
+        return self.reduce(sig)
+
+    def test_install(self) -> None:
+        func = self.make_test_1()
+        assert {"a": "1", "b": 2} == func.install(a="1", b=2)
+        assert {"a": "1", "b": 2} == func.install("1", b=2)
+        assert {"a": "1", "b": 2} == func.install("1", 2)
+        assert {"a": "1", "b": None} == func.install(a="1")
+        assert {"a": "1", "b": None} == func.install("1")
+
+    def test_bad_install_too_many_arguments(self):
+        func = self.make_test_1()
+        with pytest.raises(TypeError):
+            # Too Many Arguments
+            func.install("1", 2, 3)
+
+    def test_bad_install_too_little_arguments(self):
+        func = self.make_test_1()
+        with pytest.raises(BaseException):
+            # Too Little Arguments
+            func.install()
+
+    def test_bad_install_overlapping(self):
+        func = self.make_test_1()
+        with pytest.raises(TypeError):
+            # Overlapping tuple and dict keywords
+            func.install("1", a="10")
+
+    def test_calling_returnables(self, addition_cases: tuple[int, ...]):
+        def addition_cb(a: int, b: int):
+            return a + b
+
+        a, b, c = addition_cases
+        func = self.reduce(addition_cb)
+        assert func({"a": a, "b": b}) == c
+
+    def test_call_raises(self):
+        class Problem(Exception):
+            pass
+
+        def raise_me():
+            raise Problem("problem...")
+
+        func = self.reduce(raise_me)
+
+        with pytest.raises(Problem, match=r"problem..."):
+            func({})
+
+    def test_call_with_unwanted_arguments(self):
+        def i_require_foo(foo: str):
+            assert foo == "SPAM"
+
+        func = self.reduce(i_require_foo)
+        # It's what reduce's job is intended to do.
+        # Allow creating pluggy-like systems but at a
+        # very low level...
+        func({"foo": "SPAM", "extra": "BLAH BLAH BLAH"})
+
+    def test_is_reducable_check(self):
+        assert is_reducable(self.make_test_1())
+        assert is_reducable(0xDEAD) is False
+
+
+class TestPyReduce(BaseTestReduce):
+    reduce = reduce_py
+
+
+class TestCReduce(BaseTestReduce):
+    reduce = reduce_c
